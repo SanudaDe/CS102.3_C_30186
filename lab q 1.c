8c357686fa609cb350513f913e8ba10a1be9e9ec
SanudaDe <135825581+SanudaDe@users.noreply.github.com>
2023-06-26 16:54:57 +0530

    Add files via upload

---
+++ lab q 1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdio.h>
+int main()
+{
+    for(int x=1; x<=100; x++)
+    {
+        printf("%d ",x);
+        }
+
+        printf("\n\n");
+        int y=1;
+        while(y<=100);
+        {
+        printf("%d ",y);
+        y++;
+        }
+
+    printf("\n\n");
+     int z=1;
+     do{
+        printf("%d ",z);
+        z++;
+    }while(z<=100);
+
+    return 0;
+
+
+}
